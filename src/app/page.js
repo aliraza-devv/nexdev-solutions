028c5c8902643d9906b6d2334e46b4f319a14e36
aliraza-devv <alirazaa.dev.web@gmail.com>
2024-05-24 01:54:23 +0500

    changed title

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Logo from "../Assets/Logos/nexdev-logo-full.png";
 
 export default function Home() {
   return (
-    <main className="flex w-[100vw] h-[100vh] flex-col items-center p-24 bg-[#1a1728]">
+    <main className="flex w-[100vw] h-[100vh] flex-col items-center p-24 bg-[#161616]">
       <Image src={Logo} alt="Nexdev Solutions Logo" width={250} height={250} />
       <h1 className="text-[#f5f5f5] font-extrabold text-6xl">Launching Soon</h1>
       <div className="🤚 mt-16">
