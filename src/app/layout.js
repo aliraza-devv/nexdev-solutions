cf94605ce34b9a0ba5ef559f0f9d798d8f9e1086
aliraza-devv <alirazaa.dev.web@gmail.com>
2024-08-17 17:39:52 +0500

    added vercel speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children} 
+        {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
